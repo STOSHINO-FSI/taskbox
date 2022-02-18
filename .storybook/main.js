2a75f1df294aec406bdc01a74da110bde85399e9
坂田 駿允 <stoshino@fsi.co.jp>
2022-02-18 17:37:12 +0900

    component task

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,8 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
+  //👇 Location of our stories
+  stories: ['../src/components/**/*.stories.js'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
+  core: {
+    builder: "webpack5"
   }
-}
+};
